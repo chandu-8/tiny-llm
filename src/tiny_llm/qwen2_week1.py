15b2622ede774e34071b0b8b26eb43f6e4a1a1d4
chandu-8 <hemachandra1410@gmail.com>
2025-08-30 13:26:34 +0300

    grouped query attention for qwen2

--- src/tiny_llm/qwen2_week1.py
+++ src/tiny_llm/qwen2_week1.py
@@ -24,14 +24,52 @@ class Qwen2MultiHeadAttention:
         max_seq_len: int = 32768,
         theta: int = 1000000,
     ):
-        pass
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+        self.num_kv_heads = num_kv_heads
+        self.head_dim = hidden_size // num_heads
+        self.wq = wq
+        self.wk = wk
+        self.wv = wv
+        self.wo = wo
+        self.bq = bq
+        self.bk = bk
+        self.bv = bv
+        self.max_seq_len = max_seq_len
+        self.theta = theta
+        self.RoPE = RoPE(dims=self.head_dim, seq_len=self.max_seq_len, base=self.theta)
+
+        # x: B, L, E
+        # q = linear(x, wq, bq) -> B, L, H_q, D
+        # k = linear(x, wk, bk) -> B, L, H, D
+        # v = linear(x, wv, bv) -> B, L, H, D
+        # q = rope(q, offset=slice(0, L))
+        # k = rope(k, offset=slice(0, L))
+        # (transpose as needed)
+        # x = scaled_dot_product_attention_grouped(q, k, v, scale, mask) -> B, L, H_q, D ; Do this at float32 precision
+        # (transpose as needed)
+        # x = linear(x, wo) -> B, L, E
 
     def __call__(
         self,
         x: mx.array,
         mask: mx.array | str | None = None,
     ) -> mx.array:
-        pass
+        B, L, E = x.shape # E = 32
+
+        q = linear(x, self.wq, self.bq).reshape(B, L, self.num_heads, self.head_dim)
+        k = linear(x, self.wk, self.bk).reshape(B, L, self.num_kv_heads, self.head_dim)
+        v = linear(x, self.wv, self.bv).reshape(B, L, self.num_kv_heads, self.head_dim)
+
+        q = self.RoPE(q, slice(0,L))
+        k = self.RoPE(k, slice(0,L))
+
+        q = q.transpose((0, 2, 1, 3))
+        k = k.transpose((0, 2, 1, 3))
+        v = v.transpose((0, 2, 1, 3))
+        x = scaled_dot_product_attention_grouped(q.astype(mx.float32), k.astype(mx.float32), v.astype(mx.float32), mask=mask).transpose((0, 2, 1, 3))
+        x = x.reshape((B, L, E))
+        return linear(x, self.wo)
 
 
 class Qwen2MLP:
